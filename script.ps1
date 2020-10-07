aedfd47c5072a63ec196498581a0c9bac645b735
Avijeet Pandey <avijeetpandey25@gmail.com>
2020-10-07 23:26:00 +0530

    added backup support

--- script.ps1
+++ script.ps1
@@ -1,16 +1,17 @@
 #Path in which files will be created
 $PATH = "D:/"
-$FILE_PATH = $PATH+"/files"
-$BACKUP_PATH = $PATH+"/backup"
+$FILE_PATH = $PATH + "/files"
+$BACKUP_PATH = $PATH + "/backup"
 $FOLDERS_LIST = "week" , "month" , "daily"
 # Setting location to move to the desired directory
 Set-Location $PATH
 # Check if file already Exists
-if((Test-Path $FILE_PATH) -And (Test-Path $BACKUP_PATH))
-{
+if ((Test-Path $FILE_PATH) -And (Test-Path $BACKUP_PATH)) {
     ## If folders exist the simply zip the content and copy them to the backupfolder
-}else{
-# create the file if it is non-existent already
+    Compress-Archive -Path D:\files\*.* -DestinationPath D:\backup\daily\backup.zip
+}
+else {
+    # create the file if it is non-existent already
     mkdir $FILE_PATH
     mkdir $BACKUP_PATH
 
@@ -20,6 +21,6 @@ if((Test-Path $FILE_PATH) -And (Test-Path $BACKUP_PATH))
     #creating the backup folders
     for ($i = 0; $i -le ($FOLDERS_LIST.length - 1); $i += 1) {
         mkdir $FOLDERS_LIST[$i]
-      }
+    }
 
 }
