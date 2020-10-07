712b1c4648c28f8c6246c505e17a126fb6794dd0
Avijeet Pandey <avijeetpandey25@gmail.com>
2020-10-07 23:18:26 +0530

    added folder creation && saftey check

---
+++ script.ps1
@@ -0,0 +1,25 @@
+#Path in which files will be created
+$PATH = "D:/"
+$FILE_PATH = $PATH+"/files"
+$BACKUP_PATH = $PATH+"/backup"
+$FOLDERS_LIST = "week" , "month" , "daily"
+# Setting location to move to the desired directory
+Set-Location $PATH
+# Check if file already Exists
+if((Test-Path $FILE_PATH) -And (Test-Path $BACKUP_PATH))
+{
+    ## If folders exist the simply zip the content and copy them to the backupfolder
+}else{
+# create the file if it is non-existent already
+    mkdir $FILE_PATH
+    mkdir $BACKUP_PATH
+
+    # move to the backupfolder and create backup folders
+    Set-Location $BACKUP_PATH
+
+    #creating the backup folders
+    for ($i = 0; $i -le ($FOLDERS_LIST.length - 1); $i += 1) {
+        mkdir $FOLDERS_LIST[$i]
+      }
+
+}
